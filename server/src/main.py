d399b78a0996f459007588da3cc7c83cb2e20e18
Ah-oy <58558581+ei-blue@users.noreply.github.com>
2025-03-27 21:05:35 -0700

    add dotenv

--- server/src/main.py
+++ server/src/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
-
+from dotenv import load_dotenv
+load_dotenv()
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import ORJSONResponse
