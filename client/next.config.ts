25e4da61e173407e86ba06b16ceb0a291e510a72
shgtkshruch <shgtk.shruch@gmail.com>
2025-03-28 13:31:34 +0900

    Add static build command

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,9 @@
 import type {NextConfig} from 'next'
 
+const isStaticExport = process.env.NEXT_PUBLIC_OUTPUT_MODE === 'export'
 
 const nextConfig: NextConfig = {
+  output: isStaticExport ? 'export' : undefined,
   experimental: {
     optimizePackageImports: ['@chakra-ui/react'],
   }
