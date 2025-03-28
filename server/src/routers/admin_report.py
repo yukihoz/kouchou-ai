334d0748f4a168de6dc4ce5594dde5b43f875ea0
Ah-oy <58558581+ei-blue@users.noreply.github.com>
2025-03-28 06:40:39 -0700

    move get-csv endpoint to admin page

--- server/src/routers/admin_report.py
+++ server/src/routers/admin_report.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from fastapi import APIRouter, Depends, HTTPException, Security
-from fastapi.responses import ORJSONResponse
+from fastapi.responses import ORJSONResponse, FileResponse
 from fastapi.security.api_key import APIKeyHeader
 from src.config import settings
 from src.schemas.admin_report import ReportInput
@@ -63,3 +63,13 @@ async def create_report(report: ReportInput, api_key: str = Depends(verify_admin
     except Exception as e:
         slogger.error(f"Exception: {e}", exc_info=True)
         raise HTTPException(status_code=500, detail="Internal server error") from e
+
+
+@router.get("/get-csv/{slug}")
+async def get_csv(slug: str, api_key: str = Depends(verify_admin_api_key)):
+    # f"outputs/{config['output_dir']}/final_result_with_comments.csv"
+    csv_path = settings.REPORT_DIR / slug / "final_result_with_comments.csv"
+    if not csv_path.exists():
+        raise HTTPException(status_code=404, detail="CSV file not found")
+
+    return FileResponse(path=str(csv_path), media_type="text/csv", filename=f"kouchou_{slug}.csv")
--- server/src/routers/report.py
+++ server/src/routers/report.py
@@ -2,7 +2,6 @@ import json
 import logging
 
 from fastapi import APIRouter, Depends, HTTPException, Security
-from fastapi.responses import FileResponse
 from fastapi.security.api_key import APIKeyHeader
 from src.config import settings
 from src.schemas.report import Report, ReportStatus
@@ -45,16 +44,6 @@ async def report(slug: str, api_key: str = Depends(verify_public_api_key)) -> di
         report_result = json.load(f)
 
     return report_result
-
-
-@router.get("/get-csv/{slug}")
-async def get_csv(slug: str, api_key: str = Depends(verify_public_api_key)):
-    # f"outputs/{config['output_dir']}/final_result_with_comments.csv"
-    csv_path = settings.REPORT_DIR / slug / "final_result_with_comments.csv"
-    if not csv_path.exists():
-        raise HTTPException(status_code=404, detail="CSV file not found")
-
-    return FileResponse(path=str(csv_path), media_type="text/csv", filename=f"kouchou_{slug}.csv")
 
 
 @router.get("/test-error")
