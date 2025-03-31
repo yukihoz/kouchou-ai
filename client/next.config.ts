7e11954d5b55e169c92bee39084f735a42131691
shgtkshruch <shgtk.shruch@gmail.com>
2025-03-31 16:50:08 +0900

    Fix 404 error when reload page in static build

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,6 +4,7 @@ const isStaticExport = process.env.NEXT_PUBLIC_OUTPUT_MODE === 'export'
 
 const nextConfig: NextConfig = {
   output: isStaticExport ? 'export' : undefined,
+  trailingSlash: isStaticExport ? true : undefined,
   experimental: {
     optimizePackageImports: ['@chakra-ui/react'],
   }
