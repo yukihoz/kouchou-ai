42054aacfd99eadfb69a73f999374cf46d4e5824
shgtkshruch <shgtk.shruch@gmail.com>
2025-03-31 16:33:13 +0900

    Extract getClusterNum to utils

---
+++ client/app/utils/cluster-num.ts
@@ -0,0 +1,9 @@
+import {Result} from '@/type'
+
+export function getClusterNum(result: Result): Record<number, number> {
+  const array = result.clusters.map(c => c.level)
+  return array.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1
+    return acc
+  }, {} as Record<number, number>)
+}
