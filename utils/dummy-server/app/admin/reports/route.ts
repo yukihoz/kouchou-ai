a7d15ecc0c979b0b84b98b24d5b510634fbf70c1
shgtkshruch <shgtk.shruch@gmail.com>
2025-04-06 17:16:49 +0900

    Apply Biome format

--- utils/dummy-server/app/admin/reports/route.ts
+++ utils/dummy-server/app/admin/reports/route.ts
@@ -1,65 +1,67 @@
 export async function GET(request: Request) {
-  const requestApiKey = request.headers.get('x-api-key')
-  const validApiKey = process.env.ADMIN_API_KEY
+  const requestApiKey = request.headers.get("x-api-key");
+  const validApiKey = process.env.ADMIN_API_KEY;
   if (!requestApiKey || requestApiKey !== validApiKey) {
     return new Response(null, {
       status: 401,
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      }
-    })
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
   }
   // admin では全てのステータスのレポートを返す
   const data = [
     {
-      slug: 'example',
-      status: 'ready',
-      title: '[テスト]人類が人工知能を開発・展開する上で、最優先すべき課題は何でしょうか？',
-      description: 'あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。',
+      slug: "example",
+      status: "ready",
+      title:
+        "[テスト]人類が人工知能を開発・展開する上で、最優先すべき課題は何でしょうか？",
+      description:
+        "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。",
       isPubcom: true,
     },
     {
-      slug: 'processing',
-      status: 'processing',
-      title: '[テスト]出力中のレポート',
+      slug: "processing",
+      status: "processing",
+      title: "[テスト]出力中のレポート",
     },
-  ]
+  ];
   return new Response(JSON.stringify(data), {
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
     },
-  })
+  });
 }
 
 export async function POST(request: Request) {
-  const requestApiKey = request.headers.get('x-api-key')
-  const validApiKey = process.env.ADMIN_API_KEY
+  const requestApiKey = request.headers.get("x-api-key");
+  const validApiKey = process.env.ADMIN_API_KEY;
   if (!requestApiKey || requestApiKey !== validApiKey) {
     return new Response(null, {
       status: 401,
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      }
-    })
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
   }
   return new Response(null, {
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
     },
-  })
+  });
 }
 
 export async function OPTIONS() {
   return new Response(null, {
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, x-api-key'
-    }
-  })
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, x-api-key",
+    },
+  });
 }
--- utils/dummy-server/app/reports/route.ts
+++ utils/dummy-server/app/reports/route.ts
@@ -1,40 +1,42 @@
 export async function GET(request: Request) {
-  const requestApiKey = request.headers.get('x-api-key')
-  const validApiKey = process.env.PUBLIC_API_KEY
+  const requestApiKey = request.headers.get("x-api-key");
+  const validApiKey = process.env.PUBLIC_API_KEY;
   if (!requestApiKey || requestApiKey !== validApiKey) {
     return new Response(null, {
       status: 401,
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      }
-    })
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
   }
   // reports では ready ステータスのレポートだけを返す
   const data = [
     {
-      slug: 'example',
-      status: 'ready',
-      title: '[テスト]人類が人工知能を開発・展開する上で、最優先すべき課題は何でしょうか？',
-      description: 'あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。',
+      slug: "example",
+      status: "ready",
+      title:
+        "[テスト]人類が人工知能を開発・展開する上で、最優先すべき課題は何でしょうか？",
+      description:
+        "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。",
       isPubcom: true,
-    }
-  ]
+    },
+  ];
   return new Response(JSON.stringify(data), {
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
     },
-  })
+  });
 }
 
 export async function OPTIONS() {
   return new Response(null, {
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, x-api-key'
-    }
-  })
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, x-api-key",
+    },
+  });
 }
